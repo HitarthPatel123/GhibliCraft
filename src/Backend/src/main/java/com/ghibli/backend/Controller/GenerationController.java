522c72d1e982d9958743daee5b9b24164162fb5c
HitarthPatel123 <hitarthpadaliya@gmail.com>
2025-07-04 11:20:55 +0530

    Backend Controller

--- src/Backend/src/main/java/com/ghibli/backend/Controller/GenerationController.java
+++ src/Backend/src/main/java/com/ghibli/backend/Controller/GenerationController.java
@@ -1,4 +1,41 @@
 package com.ghibli.backend.Controller;
 
+import com.ghibli.backend.DTO.TextGenerationRequest;
+import com.ghibli.backend.Service.GhibliArtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+@RequiredArgsConstructor
 public class GenerationController {
+
+    private final GhibliArtService ghibliArtService;
+
+    @PostMapping(value = "/generate", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateGhibliArt(@RequestParam("image") MultipartFile image,
+                                                    @RequestParam("prompt") String prompt) {
+        try{
+            byte[] imageBytes = ghibliArtService.createGhibliArt(image, prompt);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(imageBytes);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping(value = "/generate-from-text", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateGhibliArtFromText(@RequestBody TextGenerationRequest requestDTO) {
+        try{
+            byte[] imageBytes = ghibliArtService.createGhibliArtFromText(requestDTO.getPrompt(), requestDTO.getStyle());
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(imageBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
